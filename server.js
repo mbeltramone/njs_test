303d54b91601a78de9fb5d561ba81c9a21d40122
mbeltramone <51237064+mbeltramone@users.noreply.github.com>
2021-06-28 10:30:31 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'application/json'});
-    res.end('{'Nombre':'Hola mundo'}');
+    res.end("{'Nombre':'Hola mundo'}");
 });
 server.listen(8000);
