bedd3c29b63b940a454aaa2fdf019b7868fad5ee
mbeltramone <51237064+mbeltramone@users.noreply.github.com>
2021-06-28 09:38:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const server = http.createServer(function (req, res) {
     res.writeHead(200, {'content-type': 'text/plain'});
-    res.end('Hola Mundo');
+    res.end('Hola Mundo 2');
 });
 server.listen(8000);
